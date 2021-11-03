2aa5d02097f55bf7f7908d5d378e836b02f2f53c
Bart <bart.rybak@hotmail.com>
2021-11-03 23:51:09 +0000

    Feature scaling using standardisation technique

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ x[:, 1:3] = imputer.transform(x[:, 1:3])
 
 # print(x)
 
+# import data transformation package
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 
@@ -35,7 +36,16 @@ y = le.fit_transform(y)
 from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+# print(x_train)
+# print(x_test)
+# print(y_train)
+# print(y_test)
+
+# Feature scaling using standardisation technique
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
+x_test[:, 3:] = sc.transform(x_test[:, 3:])
+
 print(x_train)
 print(x_test)
-print(y_train)
-print(y_test)
